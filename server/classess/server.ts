a003bc8783885bdb6a9bae92b13de2e16fcacd3f
jrodriguezweb <jesus.jarp@gmail.com>
2019-07-24 17:54:39 +0200

    adding socket.io

--- server/classess/server.ts
+++ server/classess/server.ts
@@ -1,14 +1,33 @@
 import express from 'express';
 import { SERVER_PORT } from './../global/environment';
-
+import socketIO from 'socket.io';
+import http from 'http';
 export default class Server {
-
+    
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private static _instance: Server;
+    private httpServer: http.Server;
 
-    constructor() {
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+        this.escucharSockets();
+    }
+
+    public static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets() {
+        console.log('escuchando sockets');
+
+        this.io.on('connection', cliente => {
+            console.log('cliente conectado');
+        });
     }
 
     start( callback:  () => void) {
--- server/dist/classess/server.js
+++ server/dist/classess/server.js
@@ -5,10 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("./../global/environment");
+const socket_io_1 = __importDefault(require("socket.io"));
+const http_1 = __importDefault(require("http"));
 class Server {
     constructor() {
         this.app = express_1.default();
         this.port = environment_1.SERVER_PORT;
+        this.httpServer = new http_1.default.Server(this.app);
+        this.io = socket_io_1.default(this.httpServer);
+        this.escucharSockets();
+    }
+    static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+    escucharSockets() {
+        console.log('escuchando sockets');
+        this.io.on('connection', cliente => {
+            console.log('cliente conectado');
+        });
     }
     start(callback) {
         this.app.listen(this.port, callback);

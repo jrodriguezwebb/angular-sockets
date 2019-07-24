794643ad0cae1a7cd36cb9c32c158402c0915913
jrodriguezweb <jesus.jarp@gmail.com>
2019-07-24 16:22:07 +0200

    adding cors and routes

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,3 +1,16 @@
 "use strict";
-const nombre = "Pedro";
-console.log(`Mi nombre es ${nombre}`);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./classess/server"));
+const router_1 = __importDefault(require("./routes/router"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const server = new server_1.default();
+server.app.use();
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+server.app.use('/', router_1.default);
+server.start(() => {
+    console.log(`Servidor corriendo en el puerto: ${server.port}`);
+});
---
+++ server/dist/routes/router.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = express_1.Router();
+router.get('/mensajes', (req, res) => {
+    res.json({
+        ok: true,
+        mensajes: 'Todo esta bien'
+    });
+});
+router.post('/mensajes/:id', (req, res) => {
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+    res.json({
+        ok: true,
+        cuerpo,
+        de,
+        id
+    });
+});
+exports.default = router;
